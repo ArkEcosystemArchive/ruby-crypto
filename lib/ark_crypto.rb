03f67ee2a317c54b75ea0d76fb59a4eb38b4c03e
Brian Faust <hello@brianfaust.me>
2018-06-19 10:05:07 +0300

    feat: message signing, resolves #2

---
+++ lib/ark_crypto.rb
@@ -0,0 +1,10 @@
+require 'ark_crypto/crypto'
+require 'ark_crypto/message'
+
+module ArkCrypto
+  class << self
+    def initialize
+      print "Hello World"
+    end
+  end
+end
---
+++ lib/ark_crypto/crypto.rb
@@ -0,0 +1,27 @@
+require 'btcruby'
+
+module ArkCrypto
+  class Crypto
+    def self.get_key(secret)
+      BTC::Key.new(:private_key => Digest::SHA256.digest(secret), :public_key_compressed => true)
+    end
+
+    def self.get_address(key, network_address = '17')
+      BTC::Base58.base58check_from_data([network_address.to_i(16)].pack('c') + Digest::RMD160.digest(key.public_key))
+    end
+
+    def self.get_public_key(public_key)
+      BTC::Key.new(:public_key => public_key)
+    end
+
+    def self.verify(transaction)
+      public_only_key = BTC::Key.new(:public_key => [transaction.sender_public_key].pack('H*'))
+      public_only_key.verify_ecdsa_signature([transaction.signature].pack('H*'), Digest::SHA256.digest(transaction.to_bytes))
+    end
+
+    def self.second_verify(transaction, second_public_key_hex)
+      public_only_key = BTC::Key.new(:public_key => [second_public_key_hex].pack('H*'))
+      public_only_key.verify_ecdsa_signature([transaction.sign_signature].pack('H*'), Digest::SHA256.digest(transaction.to_bytes(false)))
+    end
+  end
+end
