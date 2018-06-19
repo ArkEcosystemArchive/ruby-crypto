c420afde96cb405094dca9341e4d68ea79a2de7b
Brian Faust <hello@brianfaust.me>
2018-06-19 13:31:12 +0300

    test: remove useless define

--- spec/ark_crypto/transactions/transfer_spec.rb
+++ spec/ark_crypto/transactions/transfer_spec.rb
@@ -10,33 +10,31 @@ describe ArkCrypto::Transactions::Transfer do
   let(:secret) { 'this is a top secret passphrase' }
   let(:second_secret) { 'this is a top secret second passphrase' }
 
-  describe '#create' do
-    it 'should be ok with a secret' do
-      transaction = described_class.new
-      .recipient_id(recipient_id)
-      .amount(amount)
-      .vendor_field(vendor_field)
-      .create
-      .sign(secret)
-      .get_struct
+  it 'should be ok with a secret' do
+    transaction = described_class.new
+    .recipient_id(recipient_id)
+    .amount(amount)
+    .vendor_field(vendor_field)
+    .create
+    .sign(secret)
+    .get_struct
 
-      expect(ArkCrypto::Crypto.verify(transaction)).to be_truthy
-    end
+    expect(ArkCrypto::Crypto.verify(transaction)).to be_truthy
+  end
 
-    it 'should be ok with a second secret' do
-      transaction = described_class.new
-      .recipient_id(recipient_id)
-      .amount(amount)
-      .vendor_field(vendor_field)
-      .create
-      .sign(secret)
-      .second_sign(second_secret)
-      .get_struct
+  it 'should be ok with a second secret' do
+    transaction = described_class.new
+    .recipient_id(recipient_id)
+    .amount(amount)
+    .vendor_field(vendor_field)
+    .create
+    .sign(secret)
+    .second_sign(second_secret)
+    .get_struct
 
-      second_public_key_address = ArkCrypto::Crypto.get_key(second_secret).public_key.unpack('H*').first
+    second_public_key_address = ArkCrypto::Crypto.get_key(second_secret).public_key.unpack('H*').first
 
-      expect(ArkCrypto::Crypto.verify(transaction)).to be_truthy
-      expect(ArkCrypto::Crypto.second_verify(transaction, second_public_key_address)).to be_truthy
-    end
+    expect(ArkCrypto::Crypto.verify(transaction)).to be_truthy
+    expect(ArkCrypto::Crypto.second_verify(transaction, second_public_key_address)).to be_truthy
   end
 end
