a9528fea0ace6641a8b4ecf9170a8757511ab69d
Brian Faust <hello@brianfaust.me>
2018-06-27 13:05:45 +0300

    feat: second signature registration deserialiser

--- lib/ark_crypto/deserialisers/second_signature_registration.rb
+++ lib/ark_crypto/deserialisers/second_signature_registration.rb
@@ -4,7 +4,13 @@ module ArkCrypto
   module Deserialisers
     class SecondSignatureRegistration < Deserialiser
       def handle(asset_offset, transaction)
-        transaction
+        transaction[:asset] = {
+          signature: {}
+        }
+
+        transaction[:asset][:signature][:public_key] = @serialized[asset_offset, 66]
+
+        ArkCrypto::Crypto::parse_signatures(@serialized, transaction, asset_offset + 66)
       end
     end
   end
