19b98dfb91219a71f80a2e9989bf0f008212cca0
Brian Faust <hello@brianfaust.me>
2018-06-27 13:06:40 +0300

    feat: delegate resignation deserialiser

--- lib/ark_crypto/deserialisers/delegate_resignation.rb
+++ lib/ark_crypto/deserialisers/delegate_resignation.rb
@@ -4,7 +4,7 @@ module ArkCrypto
   module Deserialisers
     class DelegateResignation < Deserialiser
       def handle(asset_offset, transaction)
-        transaction
+        ArkCrypto::Crypto::parse_signatures(@serialized, transaction, asset_offset)
       end
     end
   end
