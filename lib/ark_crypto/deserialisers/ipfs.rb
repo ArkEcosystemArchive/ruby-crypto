f2c7c1b6f7217d937591eb6d88768e91950dbf75
Brian Faust <hello@brianfaust.me>
2018-06-27 13:13:20 +0300

    feat: ipfs deserialiser

--- lib/ark_crypto/deserialisers/ipfs.rb
+++ lib/ark_crypto/deserialisers/ipfs.rb
@@ -4,7 +4,13 @@ module ArkCrypto
   module Deserialisers
     class IPFS < Deserialiser
       def handle(asset_offset, transaction)
-        raise NotImplementedError
+        transaction[:asset] = {}
+
+        dag_length = @binary.unpack("C#{asset_offset / 2}Q<").last & 0xff
+
+        transaction[:asset][:dag] = @serialized[asset_offset + 2, dag_length * 2]
+
+        ArkCrypto::Crypto::parse_signatures(@serialized, transaction, asset_offset + 2 + length * 2)
       end
     end
   end
