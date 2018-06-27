f9020cf491a55f7aecae668717629cd7cbd21d1e
Brian Faust <hello@brianfaust.me>
2018-06-27 07:08:03 +0300

    feat: private key identity

---
+++ lib/ark_crypto/identity/private_key.rb
@@ -0,0 +1,11 @@
+require 'btcruby'
+
+module ArkCrypto
+  module Identity
+    class PrivateKey
+      def self.from_secret(secret)
+        BTC::Key.new(:private_key => Digest::SHA256.digest(secret), :public_key_compressed => true)
+      end
+    end
+  end
+end
