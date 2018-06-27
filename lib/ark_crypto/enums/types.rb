00766a07a7b8d49bea1547405451365e457675d6
Brian Faust <hello@brianfaust.me>
2018-06-27 15:14:07 +0300

    feat: fee configuration

--- lib/ark_crypto/enums/types.rb
+++ lib/ark_crypto/enums/types.rb
@@ -6,6 +6,10 @@ module ArkCrypto
       DELEGATE_REGISTRATION = 2
       VOTE = 3
       MULTI_SIGNATURE_REGISTRATION = 4
+      IPFS = 5
+      TIMELOCK_TRANSFER = 6
+      MULTI_PAYMENT = 7
+      DELEGATE_RESIGNATION = 8
     end
   end
 end
