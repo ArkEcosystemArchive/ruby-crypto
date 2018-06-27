7d2169ea74c96db3a8a87e33f924a280717b8ece
Brian Faust <hello@brianfaust.me>
2018-06-27 14:40:57 +0300

    feat: multi payment serialiser

--- lib/ark_crypto/serialisers/multi_payment.rb
+++ lib/ark_crypto/serialisers/multi_payment.rb
@@ -4,7 +4,17 @@ module ArkCrypto
   module Serialisers
     class MultiPayment < Serialiser
       def handle(bytes)
-        raise NotImplementedError
+        bytes << [@transaction[:asset][:payments].count].pack('Q<')
+
+        @transaction[:asset][:payments].each do |item|
+          bytes << [@item[:amount]].pack('Q<')
+
+          recipient_id = BTC::Base58.data_from_base58check(@item[:recipientId])
+          recipient_id = BTC::Data.hex_from_data(recipient_id)
+          bytes << [recipient_id].pack('H*')
+        end
+
+        bytes
       end
     end
   end
