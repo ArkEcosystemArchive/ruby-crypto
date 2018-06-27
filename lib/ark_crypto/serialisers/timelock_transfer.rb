c28800f888ca4bd78d8f770d98a926163491384e
Brian Faust <hello@brianfaust.me>
2018-06-27 14:37:17 +0300

    feat: timelock transfer serialiser

--- lib/ark_crypto/serialisers/timelock_transfer.rb
+++ lib/ark_crypto/serialisers/timelock_transfer.rb
@@ -4,7 +4,15 @@ module ArkCrypto
   module Serialisers
     class TimelockTransfer < Serialiser
       def handle(bytes)
-        raise NotImplementedError
+        bytes << [@transaction[:amount]].pack('Q<')
+        bytes << [@transaction[:timelocktype]].pack('h*')
+        bytes << [@transaction[:timelock]].pack('Q<')
+
+        recipient_id = BTC::Base58.data_from_base58check(@transaction[:recipientId])
+        recipient_id = BTC::Data.hex_from_data(recipient_id)
+        bytes << [recipient_id].pack('H*')
+
+        bytes
       end
     end
   end
