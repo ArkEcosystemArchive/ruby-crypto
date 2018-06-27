d6d38e9f2db1dbd81bdb7c889d3df930d20d0b46
Brian Faust <hello@brianfaust.me>
2018-06-27 13:05:30 +0300

    feat: delegate registration deserialiser

--- lib/ark_crypto/deserialisers/delegate_registration.rb
+++ lib/ark_crypto/deserialisers/delegate_registration.rb
@@ -4,7 +4,17 @@ module ArkCrypto
   module Deserialisers
     class DelegateRegistration < Deserialiser
       def handle(asset_offset, transaction)
-        transaction
+        transaction[:asset] = {
+          delegate: {}
+        }
+
+        username_length = @binary.unpack("C#{asset_offset / 2}Q<").last & 0xff
+
+        username = @serialized[asset_offset + 2, username_length * 2]
+
+        transaction[:asset][:delegate][:username] = BTC::Data.data_from_hex(username)
+
+        ArkCrypto::Crypto::parse_signatures(@serialized, transaction, asset_offset + (username_length + 1) * 2)
       end
     end
   end
