40961db80b91c01ffb56cef1275a13f5d1feb2dd
Brian Faust <hello@brianfaust.me>
2018-06-27 07:08:29 +0300

    fix: register identity and networks

--- lib/ark_crypto.rb
+++ lib/ark_crypto.rb
@@ -1,5 +1,12 @@
 require 'ark_crypto/crypto'
 require 'ark_crypto/message'
+require 'ark_crypto/identity/address'
+require 'ark_crypto/identity/private_key'
+require 'ark_crypto/identity/public_key'
+require 'ark_crypto/identity/wif'
+require 'ark_crypto/networks/mainnet'
+require 'ark_crypto/networks/devnet'
+require 'ark_crypto/networks/testnet'
 
 module ArkCrypto
   class << self
